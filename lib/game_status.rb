ea035c618cf314f5f13c561f8d9cad3e7d7a8605
Fabienne Rauber <fabienne.rauber@gmail.com>
2018-01-30 21:47:32 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -32,7 +32,7 @@ def draw?(board)
 end
 
 def over?(board)
-  won?(board) || draw?(board) || full?(board)
+  won?(board) == true || draw?(board) == true || full?(board) == true
   puts "Check Game Over"
 end
 
